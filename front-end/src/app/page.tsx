c318219e323cebca75ad487136e3be654b2bf682
ganjargal-28 <aicode744@gmail.com>
2024-11-13 14:51:33 +0800

    up

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,5 +1,5 @@
 export default async function Home() {
-  const response = await fetch(`${process.env.BACKEND_POINT}`);
+  const response = await fetch(`https://food-delivery-u1gp.onrender.com`);
   const data = await response.json();
   console.log("res", response);
 
